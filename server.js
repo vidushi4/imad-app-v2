c7fcc5bc010efdf96a13a2b337202680e023ebe7
vidushi4 <vidushi.agrawal2015@vit.ac.in>
2017-02-08 14:26:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', fucntion(req,res) {
+app.get('/article-one', fucntion (req,res) {
    res.send('Article one was requested and will be served here'); 
 });
 
